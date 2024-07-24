bd8f528d7f5bfde4a2e56e31fef09ef34cc054f9
Intrepid27 <167183413+Intrepid27@users.noreply.github.com>
2024-07-24 13:11:30 +0200

    rectification

--- FrontEnd/js/app.js
+++ FrontEnd/js/app.js
@@ -5,6 +5,22 @@ fetch("http://localhost:5678/api/works").then(reponse=> reponse.json()).then(dat
     gallery.innerHTML= ""
 
     for(let i= 0; i< data.length; i++){
-        console.log (data[i].title)
+    
+        let figure = document.createElement ("figure")
+        let image = document.createElement ("img")
+        let figcaption = document.createElement ("figcaption")
+
+        let figcaption_texte = document.createTextNode(data[i].title)
+        figcaption.appendChild(figcaption_texte)
+        figure.appendChild(figcaption)
+
+        // attribution des elements pour nos ellements images
+        image.src = data[i].imageUrl
+        figure.appendChild(image)
+
+
+        gallery.appendChild(figure)
     }
+
+
 })
