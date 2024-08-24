49e8bbcc215f9bf1b101e2eccb8c9a0f41476baa
Intrepid27 <simontoutain80@gmail.com>
2024-08-24 11:01:43 +0200

    rectification

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,18 +1,42 @@
-const users = [
-    { email: "sophie.bluel@test.tld", password: "SOphie" }, // Exemple d'utilisateur stocké en mémoire
-];
+const form = document.querySelector('#formLogin');
+const errorMessage = document.querySelector('.errorMessage'); // !!! 
+const buttonLogin = document.querySelector('.login_button');
 
-const 
+buttonLogin.addEventListener('click', (event) => {
 
-login = (req, res) => {
-    const { email, password } = req.body;
+    event.preventDefault(); // Stop l'action par défaut
 
-    // Recherche de l'utilisateur
-    const user = users.find(u => u.email === email && u.password === password);
 
-    if (user) {
-        res.status(200).send("Connexion réussie !");
-    } else {
-        res.status(401).send("Nom d'utilisateur ou mot de passe incorrect.");
-    }
-};
+//Récupération des valeurs pour charge utile fetch('')
+const emailInput = document.getElementById('email').value;
+const passwordInput = document.getElementById('password').value;
+
+// Création de des données pour charge utile
+let dataForm = {
+    email : emailInput,
+    password : passwordInput
+}
+
+// Création de la charge utile au format JSON
+const chargeUtile = JSON.stringify(dataForm)
+
+//Appel de la fonction FETCH
+fetch ('http://localhost:5678/api/users/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+      body: chargeUtile
+    })
+    .then (data => data.json())
+    .then (data => {
+        console.log(data);
+        /*if(data.token){
+            window.localStorage.setItem('token', data.token);
+            window.location.href = 'index.html';
+        } else {
+            errorMessage.style.display = 'block';
+            console.log("Erreur dans l'identifiant ou le mot de passe");
+        }*/
+    }).catch(error => console.log(error));
+},false);
